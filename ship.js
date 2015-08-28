bf320547ca4c676249db07f758a79b0f335d35bc
Larry Wu <lw7360@gmail.com>
2015-08-28 09:32:46 -0700

    Progress for init option

--- ship.js
+++ ship.js
@@ -1,16 +1,24 @@
 #!/usr/bin/env node
 
 var argv = require('yargs').demand(1).argv;
+var expandTilde = require('expand-tilde');
 var fs = require('fs');
+var mkdirp = require('mkdirp');
 var yargs = require('yargs');
 
-
-console.log(argv);
+// console.log(argv);
 
 if (argv._.length === 1) {
   switch(argv._[0]) {
     case 'init':
       // Initialize Ship in ~/.ship
+      var shipPath = expandTilde('~') + '/.ship';
+      var success = mkdirp.sync(shipPath);
+      if (success) {
+        console.log('Ship has been initialized in ' + success);
+      } else {
+        console.log('Looks like you already initialized Ship before.');
+      }
       break;
   }
 }

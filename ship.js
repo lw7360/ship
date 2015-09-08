bef43232493882069cc0401c89a756caf6ad8658
Larry Wu <lw7360@gmail.com>
2015-09-08 09:52:21 -0700

    Some more cases

--- ship.js
+++ ship.js
@@ -14,7 +14,7 @@ var argv = require('yargs').demand(1).argv;
 
 var shipPath = expandTilde('~') + '/.ship';
 
-if (argv._.length === 1) {
+if (argv._.length) {
   switch(argv._[0]) {
     case 'init':
       // Initialize Ship at ~/.ship
@@ -32,11 +32,11 @@ if (argv._.length === 1) {
         };
 
         inquirer.prompt([gitConfirm, passphraseQuestion], function(answers) {
-          var config = sjcl.encrypt(answers.passphrase, JSON.stringify(defaults.config));
-          var ship = sjcl.encrypt(answers.passphrase, JSON.stringify(defaults.ship));
+          var defaultConfig = sjcl.encrypt(answers.passphrase, JSON.stringify(defaults.config));
+          var defaultShip = sjcl.encrypt(answers.passphrase, JSON.stringify(defaults.ship));
 
-          fs.writeFileSync(shipPath + '/config', config);
-          fs.writeFileSync(shipPath + '/ship', ship);
+          fs.writeFileSync(shipPath + '/config', defaultConfig);
+          fs.writeFileSync(shipPath + '/ship', defaultShip);
 
           if (answers.git) {
             Git.Repository.init(shipPath, 0)
@@ -52,6 +52,24 @@ if (argv._.length === 1) {
         });
       } else {
         console.log('Looks like you already initialized Ship before.');
+      }
+      break;
+    case 'add':
+      // Add a password to Ship
+      break;
+    case 'rm':
+      // Remove a password from Ship
+      break;
+    case 'generate':
+      // Generate a password
+      break;
+    case 'json':
+      // Prints out or reads in a json file that contains all passwords
+      if (argv._[1]) {
+        // Reading in
+        var newShip = JSON.parse(fs.readFileSync(argv._[1], 'utf8'));
+      } else {
+        // Printing out
       }
       break;
     default:

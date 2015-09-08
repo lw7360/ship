ff0aa7a99c3a431c46d7edf2f19d286de53e6de2
Larry Wu <lw7360@gmail.com>
2015-09-08 00:49:11 -0700

    refactored init code to use sjcl instead of openpgp

---
+++ defaults.js
@@ -0,0 +1,12 @@
+var config = {
+  timeout: 1000 * 60 * 15, // Default timeout
+}
+
+var ship = {
+
+}
+
+module.exports = {
+  config: config,
+  ship: ship,
+}
--- ship.js
+++ ship.js
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
 
-var argv = require('yargs').demand(1).argv;
-var expandTilde = require('expand-tilde');
-var fs = require('fs');
-var inquirer = require('inquirer');
-var mkdirp = require('mkdirp');
-var Promise = require('bluebird');
-var openpgp = require('openpgp');
+var expandTilde = require("expand-tilde");
+var defaults = require("./defaults.js");
+var fs = require("fs");
+var Git = require("nodegit");
+var inquirer = require("inquirer");
+var mkdirp = require("mkdirp");
+var Promise = require("bluebird");
+var sjcl = require("sjcl");
 
-Promise.promisifyAll(fs);
+var argv = require('yargs').demand(1).argv;
+  // .completion('completion').argv;
 
 var shipPath = expandTilde('~') + '/.ship';
 
 if (argv._.length === 1) {
   switch(argv._[0]) {
     case 'init':
-      // Initialize Ship in ~/.ship
+      // Initialize Ship at ~/.ship
       if (mkdirp.sync(shipPath)) {
         var gitConfirm = {
           type: 'confirm',
@@ -28,25 +30,30 @@ if (argv._.length === 1) {
           name: 'passphrase',
           message: 'Enter a super secure passphrase: ',
         };
-        
-        inquirer.prompt([gitConfirm, passphraseQuestion], function (answers) {
-          var options = {
-            numBits: 2048,
-            userId: 'Ship Key <ship.key@example.org>',
-            passphrase: answers.passphrase
- 
-          };
 
-          console.log('Generating PGP key...');
-          openpgp.generateKeyPair(options).then(function(keypair) {
-            console.log('Ship has been initialized!');
-          }).catch(function(error) {
-            console.log(error); 
-          });
+        inquirer.prompt([gitConfirm, passphraseQuestion], function(answers) {
+          var config = sjcl.encrypt(answers.passphrase, JSON.stringify(defaults.config));
+          var ship = sjcl.encrypt(answers.passphrase, JSON.stringify(defaults.ship));
+
+          fs.writeFileSync(shipPath + '/config', config);
+          fs.writeFileSync(shipPath + '/ship', ship);
+
+          if (answers.git) {
+            Git.Repository.init(shipPath, 0)
+            .then(function() {
+              console.log('Ship has been successfuly initialized with Git enabled at: ' + shipPath);
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+          } else {
+            console.log('Ship has been successfuly initialized at: ' + shipPath);
+          }
         });
       } else {
         console.log('Looks like you already initialized Ship before.');
       }
       break;
+    default:
   }
 }

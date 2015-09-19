e6e934941609fe133702a11a39b2eb07e9b50222
Larry Wu <lw7360@gmail.com>
2015-09-18 23:43:17 -0700

    Removed NodeGit dependency

--- ship.js
+++ ship.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
 var cryptohelper = require("./lib/cryptohelper");
+var exec = require("child_process").exec;
 var expandTilde = require("expand-tilde");
 var defaults = require("./lib/defaults");
 var fs = require("fs-extra");
-var Git = require("nodegit");
 var inquirer = require("inquirer");
 var mkdirp = require("mkdirp");
 
-var argv = require('yargs').argv;
+var argv = require("yargs").argv;
   // .completion('completion').argv;
 
-var shipPath = expandTilde('~') + '/.ship';
-var shipTmp = shipPath + '/tmp';
-var shipJson = shipPath + '/ship.json';
-var shipConfig = shipPath + '/config.json';
-var shipJsonEncrypted = shipPath + '/ship.json';
-var shipConfigEncrypted = shipPath + '/config.json';
+var shipPath = expandTilde('~') + '/.ship/';
+var shipTmp = shipPath + 'tmp/';
+var shipJson = shipPath + 'ship.json';
+var shipConfig = shipPath + 'config.json';
+var shipJsonEncrypted = shipPath + 'ship';
+var shipConfigEncrypted = shipPath + 'config';
 
 // Returns whether Ship has already been initialized 
 // i.e. whether ~/.ship, ~/.ship/ship.json, and ~/.ship/config.json all exist
@@ -33,7 +33,6 @@ function isShipInit() {
   }
 }
 
-
 // Initialize Ship at ~/.ship
 function initShip() {
   mkdirp.sync(shipPath);
@@ -42,21 +41,23 @@ function initShip() {
   inquirer.prompt([defaults.gitConfirm, defaults.passphraseQuestion], function(answers) {
     var keys = cryptohelper.generateKeys(answers.passphrase);
     var encryptedShip = cryptohelper.encryptObject(answers.passphrase, keys, defaults.ship);
-    var defaultConfig = cryptohelper.encryptObject(answers.passphrase, keys, defaults.config);
+    var encryptedConfig = cryptohelper.encryptObject(answers.passphrase, keys, defaults.config);
 
     fs.writeJSONSync(shipJson, defaults.ship);
     fs.writeJSONSync(shipConfig, defaults.config);
-    fs.copySync('./lib/ship.gitignore', shipPath + '/.gitignore');
+    fs.writeJSONSync(shipJsonEncrypted, encryptedShip);
+    fs.writeJSONSync(shipConfigEncrypted, encryptedConfig);
+    fs.copySync('./lib/ship.gitignore', shipPath + '.gitignore');
 
     if (answers.git) {
-      Git.Repository.init(shipPath, 0)
-      .then(function() {
-        console.log('Ship has been successfuly initialized with Git enabled at: ' + shipPath);
+      exec('cd ' + shipPath + ' && git init && git add . && git commit -m "Initial Ship commit"', function(err, stdout, stderr) {
+        if (err) {
+          console.log("Git couldn't be initialized at: " + shipPath);
+          console.log("But other than that, Ship is ready to go.");
+          return;
+        }
+        console.log('Ship has been successfuly initialized with Git at: ' + shipPath);
       })
-      .catch(function (error) {
-        console.log('Woops')
-        console.log(error);
-      });
     } else {
       console.log('Ship has been successfuly initialized at: ' + shipPath);
     }
